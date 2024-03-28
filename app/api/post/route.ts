d1de017f5397860ad8452ac1892ffa2f05d0cf6a
YoshimiTakuto <takuto.yoshimi.ncdc.co.jp>
2024-03-28 23:42:42 +0900

    feat: get post api and form page

--- app/api/post/route.ts
+++ app/api/post/route.ts
@@ -7,3 +7,16 @@ export async function GET(req: Request) {
   const allBBSPosts = await prisma.post.findMany();
   return NextResponse.json(allBBSPosts);
 }
+
+export async function POST(req: Request) {
+  const { username, title, content } = await req.json();
+
+  const post = await prisma.post.create({
+    data: {
+      username,
+      title,
+      content,
+    },
+  });
+  return NextResponse.json(post);
+}
